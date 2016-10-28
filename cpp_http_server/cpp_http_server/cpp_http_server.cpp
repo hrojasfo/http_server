5eb6267eb2d99e1df309e171cc4f27309b3adf9d
hrojasfo <herofo2040@gmail.com>
2016-10-28 08:13:26 -0600

    First upload from visual studio

---
+++ cpp_http_server/cpp_http_server/cpp_http_server.cpp
@@ -0,0 +1,15 @@
+// cpp_http_server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>;
+#include <string>;
+
+int main()
+{
+	std::string name;
+	std::cout << "Starting http server...\n";
+	std::cin >> name;
+    return 0;
+}
+
